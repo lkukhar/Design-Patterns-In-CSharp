4696c17a52a0f8d744eb42cd24077250fcc3ab1f
lkukhar <16503599+lkukhar@users.noreply.github.com>
2019-02-08 12:27:58 -0500

    Implement Singleton and LazySingleton

---
+++ SingletonPattern/SingletonImplementation/LazySingleton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SingletonImplementation
+{
+    //Credit to http://csharpindepth.com/Articles/General/Singleton.aspx for code example
+    public sealed class LazySingleton
+    {
+        private static readonly Lazy<LazySingleton> lazyInitializer = new Lazy<LazySingleton>(() => new LazySingleton());
+        private LazySingleton() { }
+
+        public static LazySingleton Instance { get { return lazyInitializer.Value; } }
+    }
+}
---
+++ SingletonPattern/SingletonImplementation/Singleton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonImplementation
+{
+    public sealed class Singleton
+    {
+        public static Singleton Instance { get; } = new Singleton();
+        static Singleton() { }
+        private Singleton() { }
+
+    }
+}
