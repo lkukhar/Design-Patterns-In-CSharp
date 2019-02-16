0c1d00667b9e23f33815ed8eb43b4738ee7840b1
lkukhar <16503599+lkukhar@users.noreply.github.com>
2019-02-15 21:30:41 -0500

    Remove And Sort Usings

--- SingletonPattern/SingletonPatternImplementation/Singleton.cs
+++ SingletonPattern/SingletonPatternImplementation/Singleton.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SingletonPatternImplementation
+﻿namespace SingletonPatternImplementation
 {
     public sealed class Singleton
     {
