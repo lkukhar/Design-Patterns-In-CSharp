df434fd9a9794c8843e832f9b3144056517cdff0
lkukhar <16503599+lkukhar@users.noreply.github.com>
2019-02-26 05:14:42 -0500

    Complete ProxyPatternImplementation And ProxyPatternTests

---
+++ StructuralPatterns/ProxyPattern/ProxyPatternTests/ProxyTest.cs
@@ -0,0 +1,22 @@
+﻿using ProxyPatternImplementation;
+using Xunit;
+
+namespace ProxyPatternTests
+{
+    public class ProxyTest
+    {
+        private CarProxy _carProxy = new CarProxy();
+
+        [Fact]
+        public void Turn_On_Through_Car_Proxy()
+        {
+            Assert.Equal("Car has been turned on.", _carProxy.TurnOn());
+        }
+
+        [Fact]
+        public void Turn_Off_Through_Car_Proxy()
+        {
+            Assert.Equal("Car has been turned off.", _carProxy.TurnOff());
+        }
+    }
+}
