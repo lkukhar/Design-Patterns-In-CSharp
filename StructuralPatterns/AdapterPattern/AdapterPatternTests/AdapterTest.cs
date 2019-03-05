b83c4b5994fe2b55c229f676bdb48a3b24e04a06
lkukhar <16503599+lkukhar@users.noreply.github.com>
2019-03-05 10:23:10 -0500

    Complete AdapterTest

---
+++ StructuralPatterns/AdapterPattern/AdapterPatternTests/AdapterTest.cs
@@ -0,0 +1,16 @@
+﻿using AdapterPatternImplementation;
+using Xunit;
+
+namespace AdapterPatternTests
+{
+    public class AdapterTest
+    {
+        [Fact]
+        public void Imperial_Adapter_Converts_To_Metric()
+        {
+            ImperialAdapter imperialAdapter = new ImperialAdapter(new Imperial(2));
+            Metric metric = new Metric(imperialAdapter.ConvertToMetric());
+            Assert.Equal(5.08, metric.Centimeters);
+        }
+    }
+}
