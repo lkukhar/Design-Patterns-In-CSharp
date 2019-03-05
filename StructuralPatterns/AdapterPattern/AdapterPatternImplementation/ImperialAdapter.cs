667e967955e5d1340175c48de71943e029b6f135
lkukhar <16503599+lkukhar@users.noreply.github.com>
2019-03-05 10:15:34 -0500

    Remove And Sort Usings

--- StructuralPatterns/AdapterPattern/AdapterPatternImplementation/ImperialAdapter.cs
+++ StructuralPatterns/AdapterPattern/AdapterPatternImplementation/ImperialAdapter.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AdapterPatternImplementation
+﻿namespace AdapterPatternImplementation
 {
     public class ImperialAdapter
     {
