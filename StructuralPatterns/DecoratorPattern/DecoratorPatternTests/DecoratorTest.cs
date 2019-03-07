1f075c00053703755b21b4b508f302f9a38c1eb9
lkukhar <16503599+lkukhar@users.noreply.github.com>
2019-03-06 21:10:02 -0500

    Complete DecoratorPatternTests

---
+++ StructuralPatterns/DecoratorPattern/DecoratorPatternTests/DecoratorTest.cs
@@ -0,0 +1,26 @@
+﻿using DecoratorPatternImplementation;
+using Xunit;
+
+namespace DecoratorPatternTests
+{
+    public class DecoratorTest
+    {
+        [Fact]
+        public void Creates_Red_Lexus()
+        {
+            Lexus lexus = new Lexus();
+            RedCarDecorator redCarDecorator = new RedCarDecorator(lexus);
+            Assert.Equal("Lexus is being driven.", redCarDecorator.Drive());
+            Assert.Equal("Red", lexus.Color);
+        }
+
+        [Fact]
+        public void Creates_Red_BMW()
+        {
+            BMW bmw = new BMW();
+            RedCarDecorator redCarDecorator = new RedCarDecorator(bmw);
+            Assert.Equal("BMW is being driven.", redCarDecorator.Drive());
+            Assert.Equal("Red", bmw.Color);
+        }
+    }
+}
